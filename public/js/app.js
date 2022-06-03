4045d421820ed5b2a84db7a622b7c948b301e81c
Kishore <anandan.kishore@gmail.com>
2022-06-03 10:56:51 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,17 +9,15 @@ weatherForm.addEventListener('submit', (event) => {
   const location = search.value;
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
-  fetch('http://localhost:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          console.log('Error message:', data.error);
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        console.log('Error message:', data.error);
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
